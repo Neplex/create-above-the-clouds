b22da8c0a22b7d9b1529d85e40fd9dd7ca726040
Neplex <nicolashiot@hotmail.fr>
2026-07-29 19:01:23 +0200

    Integrate Petrochem with Diesel Generator

--- kubejs/server_scripts/metal.js
+++ kubejs/server_scripts/metal.js
@@ -6,6 +6,8 @@ ServerEvents.tags('item', event => {
     event.add('sable:super_heavy', 'createbigcannons:steel_block')
     event.add('bits_n_bobs:super_heavy', 'createbigcannons:steel_block')
     event.add('c:plates', 'cgs:steel_sheet')
+    event.add('c:dusts/carbon', 'cgs:charcoal_dust')
+    event.add('c:dusts/carbon', 'petrochem:petroleum_coke')
 
     event.remove('c:nuggets', 'cgs:steel_nugget')
     event.remove('c:nuggets/steel', 'cgs:steel_nugget')
@@ -13,6 +15,17 @@ ServerEvents.tags('item', event => {
     event.remove('c:ingots/steel', 'cgs:steel_ingot')
     event.remove('c:storage_blocks', 'cgs:steel_block')
     event.remove('c:storage_blocks/steel', 'cgs:steel_block')
+    event.remove('c:plates', 'cgs:steel_sheet')
+    event.remove('c:plates/steel', 'cgs:steel_sheet')
+
+    event.remove('c:ingots', 'petrochem:steel_ingot')
+    event.remove('c:ingots/steel', 'petrochem:steel_ingot')
+    event.remove('c:storage_blocks', 'petrochem:steel_block')
+    event.remove('c:storage_blocks/steel', 'petrochem:steel_block')
+    event.remove('c:ingots', 'petrochem:bronze_ingot')
+    event.remove('c:ingots/bronze', 'petrochem:bronze_ingot')
+    event.remove('c:nuggets', 'petrochem:tin_nugget')
+    event.remove('c:nuggets/tin', 'petrochem:tin_nugget')
 
     event.remove('c:plates', 'powergrid:zinc_sheet')
     event.remove('c:plates/zinc', 'powergrid:zinc_sheet')
@@ -27,16 +40,25 @@ ServerEvents.recipes(event => {
     event.replaceInput({input: 'cgs:steel_ingot'}, 'cgs:steel_ingot', Ingredient.of('#c:ingots/steel'))
     event.replaceInput({input: 'cgs:steel_sheet'}, 'cgs:steel_sheet', Ingredient.of('#c:plates/steel'))
     event.replaceInput({input: 'cgs:steel_block'}, 'cgs:steel_block', Ingredient.of('#c:storage_blocks/steel'))
+    event.replaceInput({input: 'petrochem:steel_ingot'}, 'petrochem:steel_ingot', Ingredient.of('#c:ingots/steel'))
+
+    event.replaceInput({input: 'createbigcannons:bronze_ingot'}, 'createbigcannons:bronze_ingot', Ingredient.of('#c:ingots/bronze'))
+    event.replaceInput({input: 'petrochem:bronze_ingot'}, 'petrochem:bronze_ingot', Ingredient.of('#c:ingots/bronze'))
 
     // Remove the old recipes
     event.remove({output: 'cgs:steel_nugget'})
     event.remove({output: 'cgs:steel_ingot'})
     event.remove({output: 'cgs:steel_block'})
+    event.remove({output: 'cgs:steel_sheet'})
+    event.remove({output: 'petrochem:steel_ingot'})
+    event.remove({output: 'petrochem:steel_block'})
+    event.remove({output: 'petrochem:bronze_ingot'})
+    event.remove({output: 'petrochem:tin_nugget'})
     event.remove({output: 'powergrid:zinc_sheet'})
 
     // Replace coal with charcoal dust in the mixin recipe
-    event.replaceInput({type: 'create:mixing', output: '#c:ingots/steel' }, '#minecraft:coals', '#c:dusts/charcoal_dust')
-    event.replaceInput({type: 'create:compacting', output: '#c:ingots/cast_iron' }, '#minecraft:coals', '#c:dusts/charcoal_dust')
+    event.replaceInput({type: 'create:mixing', output: '#c:ingots/steel' }, '#minecraft:coals', '#c:dusts/carbon')
+    event.replaceInput({type: 'create:compacting', output: '#c:ingots/cast_iron' }, '#minecraft:coals', '#c:dusts/carbon')
 })
 
 // Hide items from recipe viewers
@@ -44,5 +66,10 @@ RecipeViewerEvents.removeEntriesCompletely('item', event => {
     event.remove('cgs:steel_nugget')
     event.remove('cgs:steel_ingot')
     event.remove('cgs:steel_block')
+    event.remove('cgs:steel_sheet')
+    event.remove('petrochem:steel_ingot')
+    event.remove('petrochem:steel_block')
+    event.remove('petrochem:bronze_ingot')
+    event.remove('petrochem:tin_nugget')
     event.remove('powergrid:zinc_sheet')
 })
---
+++ kubejs/server_scripts/oil.js
@@ -0,0 +1,61 @@
+ServerEvents.tags("fluid", (event) => {
+  event.remove("c:crude_oil", "petrochem:petroleum");
+  event.remove("c:diesel", "createdieselgenerators:diesel");
+});
+
+ServerEvents.recipes((event) => {
+  // Replace all inputs by tags
+  event.replaceInput({ input: Fluid.of("petrochem:desalted_oil") }, Fluid.of("petrochem:desalted_oil"), "#c:crude_oil");
+
+  // Replace Pterochem asphalt by diesel generator one
+  event.replaceOutput({ output: "petrochem:asphalt" }, "petrochem:asphalt", "createdieselgenerators:asphalt_block");
+  event.remove({ id: 'createdieselgenerators:crafting/asphalt_block' });
+  event.remove({ id: 'createdieselgenerators:mixing/asphalt_block' });
+
+  // Remove the old recipes
+  event.remove({ output: "petrochem:turbine" });
+  event.remove({ output: "petrochem:pumpjack_arm" });
+  event.remove({ output: "petrochem:pumpjack_crank" });
+  event.remove({ output: "petrochem:pumpjack_well" });
+  event.remove({ output: "petrochem:small_engine" });
+  event.remove({ output: "petrochem:medium_engine" });
+  event.remove({ output: "petrochem:petroleum" });
+  event.remove({ output: "petrochem:petroleum_bucket" });
+  event.remove({ output: "petrochem:desalted_oil" });
+  event.remove({ output: "petrochem:desalted_oil_bucket" });
+  event.remove({ output: "createdieselgenerators:diesel" });
+  event.remove({ output: "createdieselgenerators:diesel_bucket" });
+  event.remove({ output: "createdieselgenerators:gasoline" });
+  event.remove({ output: "createdieselgenerators:gasoline_bucket" });
+  event.remove({ output: "createdieselgenerators:distillation_controller" });
+
+  // Removes all distillation tower recipes added by Create: Diesel Generators
+  event.remove({ type: "createdieselgenerators:distillation" });
+
+  // Remove old pretoleum desalting recipe
+  event.remove({ id: 'petrochem:electrolyzing/basic_desalting' })
+});
+
+// Hide items from recipe viewers
+RecipeViewerEvents.removeEntriesCompletely("item", (event) => {
+  event.remove("petrochem:turbine");
+  event.remove("petrochem:pumpjack_arm");
+  event.remove("petrochem:pumpjack_crank");
+  event.remove("petrochem:pumpjack_well");
+  event.remove("petrochem:small_engine");
+  event.remove("petrochem:medium_engine");
+  event.remove("petrochem:petroleum_bucket");
+  event.remove("petrochem:desalted_oil_bucket");
+  event.remove("petrochem:asphalt");
+  event.remove("createdieselgenerators:diesel_bucket");
+  event.remove("createdieselgenerators:gasoline_bucket");
+  event.remove("createdieselgenerators:distillation_controller");
+});
+
+// Hide fluids from recipe viewers
+RecipeViewerEvents.removeEntriesCompletely("fluid", (event) => {
+  event.remove("petrochem:petroleum");
+  event.remove("petrochem:desalted_oil");
+  event.remove("createdieselgenerators:diesel");
+  event.remove("createdieselgenerators:gasoline");
+});
