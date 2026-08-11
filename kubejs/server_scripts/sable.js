8eaa0e0fc92390e0e2334b309c8c7fbba85b0580
Neplex <nicolashiot@hotmail.fr>
2026-08-11 21:29:41 +0200

    [POC] Sable sublevel teleport from overworld to nether with smooth air pressure
    /!\ sublevel are broken on teleport due to dimensional sable issues

--- kubejs/server_scripts/sable.js
+++ kubejs/server_scripts/sable.js
@@ -11,3 +11,46 @@ RecipeViewerEvents.removeEntriesCompletely("item", (event) => {
   event.remove(/simulated:creative_/);
   event.remove(/aeronautics_uttility_objects:creative_/);
 });
+
+const MIN_Y = -32;
+const MAX_Y = 320;
+const GAP_Y = 20
+
+ServerEvents.tick((event) => {
+  const server = event.server;
+  const container = SubLevelContainer.getContainer(
+    event.server.getLevel("minecraft:overworld"),
+  );
+
+  for (const subLevel of container.getAllSubLevels()) {
+    if (subLevel.isRemoved()) continue;
+
+    const uuid = subLevel.getUniqueId().toString();
+    const pos = subLevel.logicalPose().position();
+
+    if (pos.y < MIN_Y) {
+      server.runCommandSilent(
+        `execute in minecraft:overworld positioned ${pos.x} ${pos.y} ${pos.z} run sable dimension_set @n[limit=1,distance=..2] minecraft:the_nether ${pos.x} ${MAX_Y - GAP_Y} ${pos.z}`,
+      );
+    }
+  }
+});
+
+ServerEvents.tick((event) => {
+  const server = event.server;
+  const container = SubLevelContainer.getContainer(
+    event.server.getLevel("minecraft:the_nether"),
+  );
+  for (const subLevel of container.getAllSubLevels()) {
+    if (subLevel.isRemoved()) continue;
+
+    const uuid = subLevel.getUniqueId().toString();
+    const pos = subLevel.logicalPose().position();
+
+    if (pos.y > MAX_Y) {
+      server.runCommandSilent(
+        `execute in minecraft:the_nether positioned ${pos.x} ${pos.y} ${pos.z} run sable dimension_set @n[limit=1,distance=..2] minecraft:overworld ${pos.x} ${MIN_Y + GAP_Y} ${pos.z}`,
+      );
+    }
+  }
+});
